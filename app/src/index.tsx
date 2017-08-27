53d554f8e98abc17863d0e2d368ac8527fb37aea
cnsheafe <cnsheafe@gmail.com>
2017-08-26 22:46:14 -0600

    Moved store to separate file

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,11 +4,13 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 
-import {App} from "./components/App";
+import App from "./components/App";
 import {Signup} from "./components/Signup";
+import {Reducer} from "./reducer";
+import {store} from "./store";
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <Router>
             <div>
                 <Route exact path="/" component={App}/>
